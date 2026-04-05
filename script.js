0d66bfcaf7a292192f0aff9ea7ea5a089a163054
kaoutar <you@example.com>
2026-04-05 16:04:37 +0100

    ajouter totale apres l'addition prix pour chaque article

--- script.js
+++ script.js
@@ -77,16 +77,16 @@ function panier() {
          <div class="font-bold flex flex-col gap-2 md:gap-4">
            <p class="text-[#4949ff] text-lg md:text-xl">${title}</p>
            <p class="text-gray-600">${category}</p>
-           <p class="text-green-400 font-semibold">${price}$</p>
+           <p class="pricecalcule text-green-400 font-semibold">${price}$</p>
          </div>
          <div class="flex flex-col gap-2 mt-4 md:mt-0">
            <div class="flex gap-2 md:gap-4 items-center">
              <button id="add" class="add cursor-pointer h-10 w-10 rounded-full bg-[#4949ff] text-white flex items-center justify-center">+</button>
-             <span id="quantity" class="font-bold px-2">2</span>
+             <span id="quantity" class="font-bold px-2">1</span>
              <button id="subtract" class="subtract cursor-pointer h-10 w-10 rounded-full bg-[#4949ff] text-white flex items-center justify-center">-</button>
            </div>
            <div class="flex justify-between items-center mt-2">
-             <p class="font-bold">Total: <span class="text-green-400">$</span></p>
+             <p class=" font-bold">Total: <span class="Spantotalprice text-green-400">$</span></p>
              <img class="delete w-10 h-10 cursor-pointer" src="./images/delete.png" alt="delete">
            </div>
          </div>
@@ -100,8 +100,40 @@ function panier() {
   let quantityElement=document.querySelectorAll("#quantity")
   let subtract=document.querySelectorAll(".subtract")
   let deleteBtn=document.querySelectorAll(".delete")
+  let priceCalcule=document.querySelectorAll('.pricecalcule')
+  let Spantotalprice=document.querySelectorAll('.Spantotalprice')
+function totaleItemAdd(){
+
+  for(let i=0;i<priceCalcule.length;i++){
+    let totalPrice=0
+    totalPrice += parseFloat(priceCalcule[i].textContent) * parseInt(quantityElement[i].textContent);
+    // priceCalcule[i].textContent = `${totalPrice}$`;
+    console.log(totalPrice);
+    Spantotalprice[i].textContent = `${totalPrice.toFixed(2)}$`;
+
+  }
+}
+function totalItemMoin(){
+    for(let i=0;i<priceCalcule.length;i++){
+    let totalPrice=0
+    totalPrice += parseFloat(priceCalcule[i].textContent) - parseInt(quantityElement[i].textContent);
+    // priceCalcule[i].textContent = `${totalPrice}$`;
+    console.log(totalPrice);
+    Spantotalprice[i].textContent = `${totalPrice.toFixed(2)}$`;
+
+  }
+}
+for (let i = 0; i < add.length; i++) {
+  add[i].onclick = function() {
+    totaleItemAdd()
+    let current = parseInt(quantityElement[i].textContent);
+    quantityElement[i].textContent = current + 1;
+  };
+}
+
   for (let i = 0; i < deleteBtn.length; i++) {
     deleteBtn[i].onclick = function() {
+     
       let games = JSON.parse(localStorage.getItem("games")) || [];
       games.splice(i, 1);
       localStorage.setItem("games", JSON.stringify(games));
@@ -109,15 +141,10 @@ function panier() {
     };
     
     }
-for (let i = 0; i < add.length; i++) {
-  add[i].onclick = function() {
-    let current = parseInt(quantityElement[i].textContent);
-    quantityElement[i].textContent = current + 1;
-  };
-}
 
 for (let i = 0; i < subtract.length; i++) {
   subtract[i].onclick = function() {
+     totalItemMoin()
     let current = parseInt(quantityElement[i].textContent);
     if (current > 1) {
       quantityElement[i].textContent = current - 1;
